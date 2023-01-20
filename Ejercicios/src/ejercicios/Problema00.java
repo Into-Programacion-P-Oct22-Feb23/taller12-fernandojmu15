c3b0c50d8b43ec43ef1391af30abad7f477d8e7d
David <adamsmontoya19@gmail.com>
2023-01-20 12:03:06 -0500

    TALLER12

--- Ejercicios/src/ejercicios/Problema00.java
+++ Ejercicios/src/ejercicios/Problema00.java
@@ -16,6 +16,29 @@ public class Problema00 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        double[][] resultado = new double[4][2];
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                resultado[i][j] = factorial(datos[i][j]);
+            }
+
+        }
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("%.0f\n", resultado[i][j]);
+            }
+        }
     }
-    
+
+    public static double factorial(double numero) {
+        if (numero <= 1) {
+            return 1;
+
+        } else {
+            return numero * factorial(numero - 1);
+        }
+
+    }
 }
--- Ejercicios/src/ejercicios/Problema03.java
+++ Ejercicios/src/ejercicios/Problema03.java
@@ -16,6 +16,24 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[] bases = {100, 90, 80, 70, 60};
+        double[] potencias = {2, 3, 2, 2, 1};
+        double[] resultado = new double[5];
+
+        for (int i = 0; i < bases.length; i++) {
+            resultado[i] = obtenerPotencia(bases[i], potencias[i]);
+            System.out.printf("%.0f\t", resultado[i]);
+        }
+        System.out.println();
     }
-    
+
+    public static double obtenerPotencia(double base, double potencia) {
+
+        if (potencia == 1) {
+            return base;
+        } else {
+
+            return base * obtenerPotencia(base, potencia - 1);
+        }
+    }
 }
